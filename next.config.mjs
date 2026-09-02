94e1021d1f138707cbd7fd23c3c65603f181fc2d
areeba <areeba.dev08@gamil.com>
2026-09-02 14:24:15 +0500

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
